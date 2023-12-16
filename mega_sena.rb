e2fd7c4f1b10e767d9a3c0a7a2064487b4d0a15b
felipe <felipenuneto@gmail.com>
2023-12-16 04:06:27 -0300

    Commit_classification

--- mega_sena.rb
+++ mega_sena.rb
@@ -12,16 +12,16 @@ class Jogo
             case quantity_of_ten
             when 20
                 @numbers = (0..99).to_a
-                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+                jogo += @numbers.sample(quantity_of_ten).sort.join(" - ")
             when 5
                 @numbers = (1..80).to_a
-                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+                jogo += @numbers.sample(quantity_of_ten).sort.join(" - ")
             when 6
                 @numbers = (1..60).to_a
-                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+                jogo += @numbers.sample(quantity_of_ten).sort.join(" - ")
             when 15
                 @numbers = (1..25).to_a
-                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+                jogo += @numbers.sample(quantity_of_ten).sort.join(" - ")
             end
             jogo.concat("\n")
         end
@@ -32,19 +32,24 @@ class Jogo
 
     def nota_fiscal(quantity_of_ten, games)
         data = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+        tipo_jogo = ""
         case quantity_of_ten
         when 20
             valor = games*3
+            tipo_jogo="LOTOMANIA"
         when 5
             valor = games*2.50
+            tipo_jogo="QUINA"
         when 6
             valor = games*5
+            tipo_jogo="MEGA-SENA"
         when 15
             valor = games*3
+            tipo_jogo="LOTOFÁCIL"
         end
-        @nota = "==============================================\nDATA #{data}\nVALOR: R$#{valor}\n==============================================\n"
+        @nota = "==============================================\nDATA #{data}\nVALOR: R$#{valor}\nTIPO DO JOGO: #{tipo_jogo}\n==============================================\n"
     end
 end
 
 jogo = Jogo.new
-jogo.play(5,3)
+jogo.play(15,3)
