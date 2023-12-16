b847423d8d52c98deb85283a97b879ef4b1bde0e
felipe <felipenuneto@gmail.com>
2023-12-16 03:52:25 -0300

    Second commit

--- mega_sena.rb
+++ mega_sena.rb
@@ -1,40 +1,60 @@
-time_stamp = Time.now.strftime("%d/%m/%Y %H:%M:%S")
-if File.exist?("jogos.txt")
-    arquivo = File.write("jogos.txt","DATA: #{time_stamp}\n--------------------------")
+# time_stamp = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+# if File.exist?("jogos.txt")
+#     arquivo = File.write("jogos.txt","DATA: #{time_stamp}\n--------------------------")
 
-else
-    arquivo = File.write("jogos.txt","DATA: #{time_stamp}\n")
-    puts "Arquivo inexistente"
-end
-
-
+# else
+#     arquivo = File.write("jogos.txt","DATA: #{time_stamp}\n")
+#     puts "Arquivo inexistente"
+# end
 
 
+class Jogo
+    attr_accessor :numbers
 
-class MegaSena
-  attr_accessor :numbers
-
-  def initialize(numbers = (1..60).to_a)
-    @numbers = numbers
-  end
-
-  def play(quantity_of_ten = 6, games = 1)
-    games.times do
-      puts @numbers.sample(quantity_of_ten)
-                   .sort
-                   .map { |n| n < 10 ? "0#{n}" : n.to_s }
-                   .join(' - ')
+    def initialize(numbers = (0..99).to_a)
+        @numbers = numbers
     end
-    total(games)
-  end
 
-  def total(games)
-    value = (games * 5)
-    puts '========================================='
-    puts "TOTAL A PAGAR: R$ #{value},00"
-    puts '========================================='
-  end
+    def play(quantity_of_ten, games = 1)
+        jogo = ""
+        buffer = ""
+        games.times do
+            case quantity_of_ten
+            when 20
+                @numbers = (0..99).to_a
+                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+            when 5
+                @numbers = (1..80).to_a
+                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+            when 6
+                @numbers = (1..60).to_a
+                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+            when 15
+                @numbers = (1..25).to_a
+                jogo += @numbers.sample(quantity_of_ten).join(" - ")
+            end
+            jogo.concat("\n")
+        end
+        File.exist?("nota_fiscal.txt") ? buffer = File.read("nota_fiscal.txt") : File.write("nota_fiscal.txt","")
+        registro = nota_fiscal(quantity_of_ten, games)
+        File.write("nota_fiscal.txt", "#{buffer}\n#{registro}\n#{jogo}")
+    end
+
+    def nota_fiscal(quantity_of_ten, games)
+        data = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+        case quantity_of_ten
+        when 20
+            valor = games*3
+        when 5
+            valor = games*2.50
+        when 6
+            valor = games*5
+        when 15
+            valor = games*3
+        end
+        @nota = "==============================================\nDATA #{data}\nVALOR: R$#{valor}\n==============================================\n"
+    end
 end
 
-game = MegaSena.new
-game.play(6, 3)
+jogo = Jogo.new
+jogo.play(5,3)
